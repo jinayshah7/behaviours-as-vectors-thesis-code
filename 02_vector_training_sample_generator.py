4b8a0c1a763dd649efb40a79364d428b5a5aff2b
jinayshah <jinay.sh99@gmail.com>
2021-07-06 23:00:50 -0400

    added multiprocessing to stage 2

--- 02_vector_training_sample_generator.py
+++ 02_vector_training_sample_generator.py
@@ -1,24 +1,27 @@
+from multiprocessing import Pool
+
 from Experiment import Experiment
 from VectorTrainingSampleGenerator import VectorTrainingSampleGenerator
 from list_of_experiments import LIST_OF_EXPERIMENTS
 
 
+def process(experiment_name):
+    print(f"Current: {experiment_name}")
+    experiment = Experiment(experiment_name)
+    ve = VectorTrainingSampleGenerator(experiment)
+    ve.generate_big_graph()
+    ve.build_edge_list()
+    ve.save_edges_for_tgn()
+    ve.save_edges_for_node2vec()
+    ve.save_edges_for_line()
+    ve.save_edge_list()
+    del ve
+
+
 def main():
-
-    for experiment_number, experiment_name in enumerate(LIST_OF_EXPERIMENTS):
-
-        print(f"Completed - {experiment_number}/{len(LIST_OF_EXPERIMENTS)} - Current: {experiment_name}")
-        experiment = Experiment(experiment_name)
-
-        ve = VectorTrainingSampleGenerator(experiment)
-        ve.generate_big_graph()
-        ve.build_edge_list()
-        ve.save_edges_for_tgn()
-        ve.save_edges_for_node2vec()
-        ve.save_edges_for_line()
-        ve.save_edge_list()
+    with Pool(32) as p:
+        p.map(process, LIST_OF_EXPERIMENTS)
 
 
 if __name__ == '__main__':
     main()
-
